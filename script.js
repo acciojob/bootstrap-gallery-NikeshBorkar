1b31cb29574f4cc302e489b4594338196b3fbf6f
Nikesh Borkar <99666800+NikeshBorkar@users.noreply.github.com>
2024-06-30 14:19:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+$(document).on("click", '[data-toggle="lightbox"]', function (event) {
+        event.preventDefault();
+        $(this).ekkoLightbox();
+      });
